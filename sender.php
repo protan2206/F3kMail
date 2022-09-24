9e002577e56386ca41296578973764796944a2d6
Protan Halder <99788826+protan2206@users.noreply.github.com>
2022-09-24 11:51:07 +0600

    Add files via upload

---
+++ sender.php
@@ -0,0 +1,107 @@
+<?php
+echo '<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Mail Sender | Status</title>
+    <script src="https://kit.fontawesome.com/f85d117a1e.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+</head>
+
+<body class="bg-dark">
+
+    <div class="container">
+        <p class="text-light h2 text-center font-weight-bold">Mail Sender</p>
+        <p class="h5 text-center font-weight-bold text-uppercase" , style="color:rgb(255, 0, 0);">Created by : Protan
+            Halder</p>
+        <div class="w-100 alert alert-danger text-center" role="alert">
+            Do it for Fun. Do not MissUse it.
+        </div>
+        
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8"
+            crossorigin="anonymous"></script>
+    </div>
+</body>
+
+</html>';
+
+
+
+$to = $_GET['to'];
+$from = $_GET['from'];
+$mail_subject = $_GET['subject'];
+$mail_content = $_GET['message'];
+$header ="From: $from"."\r\n";
+$result = mail($to,$mail_subject,$mail_content,$header);
+if ($result) {
+echo '
+<div class="font-weight-bold alert alert-success" role="alert">
+    <p class="h2">Email has been Sent.</p>
+</div>';
+}
+else {
+echo '
+<div class="font-weight-bold alert alert-success" role="alert">
+    <p class="h2">Email Not been Sent.</p>
+</div>';
+}
+
+echo '
+<div class="d-flex justify-content-center">
+    <a class="btn btn-primary" href="index.html">Sent Another</a>
+</div>
+
+
+<footer class="bg-dark text-center text-white">
+    <!-- Grid container -->
+    <div class="container p-4 pb-0">
+        <!-- Section: Social media -->
+        <section class="mb-4">
+            <!-- Facebook -->
+            <a class="btn btn-outline-light btn-floating m-1" href="https://www.facebook.com/protanhalder.2/" role="button"><i
+                    class="fab fa-facebook-f"></i></a>
+
+            <!-- Twitter -->
+            <a class="btn btn-outline-light btn-floating m-1" href="https://twitter.com/ProtanHalder" role="button"><i
+                    class="fab fa-twitter"></i></a>
+
+            <!-- Google -->
+            <a class="btn btn-outline-light btn-floating m-1" href="https://protan2206.github.io/ProtanHalder/" role="button"><i
+                    class="fab fa-google"></i></a>
+
+            <!-- Instagram -->
+            <a class="btn btn-outline-light btn-floating m-1" href="https://www.instagram.com/protanhalder/" role="button"><i
+                    class="fab fa-instagram"></i></a>
+
+            <!-- Linkedin -->
+            <a class="btn btn-outline-light btn-floating m-1" href="https://bd.linkedin.com/in/protanhalder" role="button"><i
+                    class="fab fa-linkedin-in"></i></a>
+
+            <!-- Telegram -->
+            <a class="btn btn-outline-light btn-floating m-1" href="https://t.me/protan2206" role="button"><i
+                class="fab fa-brands fa-telegram"></i></a>
+
+            <!-- Github -->
+            <a class="btn btn-outline-light btn-floating m-1" href="https://github.com/protan2206/"
+                role="button"><i class="fab fa-github"></i></a>
+        </section>
+        <!-- Section: Social media -->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="rounded text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        © 2022 Copyright:
+        <a class="text-white" href="https://www.github.com/protan2206/">PROTAN HALDER</a>
+    </div>
+    <!-- Copyright -->
+</footer>
+
+';
